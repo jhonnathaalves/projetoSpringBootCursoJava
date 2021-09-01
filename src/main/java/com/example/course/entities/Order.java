fd80f6f4ef6c7d7cab3c03b925ec275464cf82d5
unknown <jhonnatha16011987@yahoo.com.br>
2021-08-31 22:32:58 -0300

    Subtotal & Total methods

--- src/main/java/com/example/course/entities/Order.java
+++ src/main/java/com/example/course/entities/Order.java
@@ -105,6 +105,14 @@ public class Order implements Serializable {
 			this.orderStatus = orderStatus.getCode();
 		}
 	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
 
 	@Override
 	public int hashCode() {
